691428d49fff6a510bd7c0aba3f17df2a3d89d7d
cyt <1512772499@qq.com>
2024-09-04 16:57:59 +0800

    添加文件夹

---
+++ exam_online/src/router/index.tsx
@@ -0,0 +1,19 @@
+import Login from "../pages/login/Login";
+import Home from "../pages/home/Home";
+
+
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/home',
+    element: <Login />
+  },
+  
+]
+
+
+
