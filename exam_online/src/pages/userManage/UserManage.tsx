2c688d455c4ec33253a8e094315062f6db61f9da
zhaoyuheng <1753649367@qq.com>
2024-09-10 22:32:55 +0800

    9.10角色管理

--- exam_online/src/pages/userManage/UserManage.tsx
+++ exam_online/src/pages/userManage/UserManage.tsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Space, Table, Button, Drawer, Tree } from 'antd'
+import type { TableProps, TreeDataNode, TreeProps } from 'antd'
+import { roleListApi, permissionListApi } from '../../services/systemap'
+import type { RoleItem } from '../../types/services/systemapi'
+import dayjs from 'dayjs'
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+}
 
-const UserManage = () => {
+type CheckedPer = {
+  checked: string[]
+  halfChecked: string[]
+}
+const System = () => {
+  const [roleList, setRoleList] = useState()
+  const [open, setOpen] = useState(false)
+  const [curRowPer, serCurRowPer] = useState<CheckedPer>({
+    checked: [],
+    halfChecked: []
+  })
+  const [permission, setPermission] = useState<any[]>([])
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false)
+  }
+  useEffect(() => {
+    roleListApi()
+      .then(res => {
+        setRoleList(res.data.data.list)
+      })
+    permissionListApi()
+      .then(res => {
+        setPermission(res.data.data.list)
+      })
+  }, [])
+
+  const columns: TableProps<RoleItem>['columns'] = [
+    {
+      title: '角色',
+      dataIndex: 'name',
+    },
+    {
+      title: '创建人',
+      dataIndex:'creator'
+    },
+    {
+      title: '创建时间',
+      dataIndex:'createTime',
+      render:(text) => {
+        return dayjs(text).format('YYYY-MM-DD HH:mm:ss')
+      }
+    },
+    {
+      title: '操作',
+      render: (_, record) => (
+        <Button type="primary" onClick={() => {
+          showDrawer()
+          const obj:CheckedPer = {
+            checked:[],
+            halfChecked:[]
+          }
+          record.permission.forEach(val => {
+            const first:{children:{_id:string}[]} = permission.find(item => item._id === val)
+            if(first){
+              if(first.children.every(v => record.permission.includes(v._id))){
+                obj.checked.push(val)
+              }else{
+                obj.halfChecked.push(val)
+              }
+            }else{
+              obj.checked.push(val)
+            }
+          })
+          serCurRowPer(obj)
+        }}>分配角色</Button>
+      )
+    }
+  ]
+
   return (
-    <div>角色管理</div>
+   <>
+      <Table columns={columns}  dataSource={roleList} rowKey="_id" />
+      <Drawer title="分配菜单" onClose={onClose} open={open} footer={<Button type="primary">确定</Button>}>
+        <Tree
+          checkable
+          checkStrictly
+          onCheck={(obj:any) => {
+            serCurRowPer(obj)
+          }}
+          checkedKeys={curRowPer}
+          treeData={permission}
+          fieldNames={{
+            key: '_id',
+            title: 'name'
+          }}
+        />
+      </Drawer>
+   </>
   )
 }
 
-export default UserManage
+export default System
